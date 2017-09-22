b265f10b4f81188b617917933a83a51330c55457
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-09-22 12:19:27 +0200

    auth form validation

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './components/login/login.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from './components/login/login.component';
 import {AuthRoutingModule} from "./auth.routing";
-import { AuthWrapperComponent } from './components/auth-wrapper/auth-wrapper.component';
+import {AuthWrapperComponent} from './components/auth-wrapper/auth-wrapper.component';
 import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    HttpModule,
     AuthRoutingModule
   ],
   declarations: [LoginComponent, AuthWrapperComponent]
 })
-export class AuthModule { }
+export class AuthModule {
+}
